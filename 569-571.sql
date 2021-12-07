e551a31401152bcfc750ea1d7bf20a78763a9c41
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2021-12-07 12:08:17 -0800

    Update and rename 569-.sql to 569-571.sql

--- 569-571.sql
+++ 569-571.sql
@@ -1,5 +1,6 @@
 --#569. Median Employee Salary
 /*
+
 Write an SQL query to find the median salary of each company.
 
 */
@@ -16,6 +17,7 @@ WHERE ranking>=cnt/2 AND ranking<=cnt/2+1
 
 --#570. Managers with at Least 5 Direct Reports
 /*
+
 Write an SQL query to report the managers with at least five direct reports.
 
 */
@@ -24,3 +26,21 @@ SELECT a.name
 FROM Employee a JOIN Employee b ON (b.ManagerId=a.Id)
 GROUP BY b.ManagerId
 HAVING COUNT(b.ManagerId)>=5
+
+
+--#571. Find Median Given Frequency of Numbers
+/*
+
+The median is the value separating the higher half from the lower half of a data sample.
+
+Write an SQL query to report the median of all the numbers in the database after decompressing the Numbers table. Round the median to one decimal point.
+*/
+
+SELECT AVG(num) median 
+FROM
+(SELECT n1.num FROM Numbers n1 JOIN Numbers n2 ON (n1.num>=n2.num )
+ GROUP BY n1.num 
+ HAVING SUM(n2.frequency)>=(SELECT SUM(frequency) FROM Numbers)/2 
+ AND SUM(n2.frequency)-AVG(n1.frequency)<=(SELECT SUM(frequency) FROM Numbers)/2
+)s
+
