4a765a642ad4fd803055b8279b459260502bdb49
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2021-12-08 11:44:02 -0800

    Update 197.sql

--- 197.sql
+++ 197.sql
@@ -3,7 +3,6 @@
 Write an SQL query to 来查找与之前（昨天的）日期相比温度更高的所有日期的id
 
 */
-
 SELECT a.id 
 FROM Weather a CROSS JOIN Weather b ON datediff(a.recordDate,b.recordDate)=1
 WHERE a.Temperature>b.Temperature
