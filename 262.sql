993fe0fd7c13fc8d7bdaef7c1c54af8035da2404
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2021-12-11 00:17:19 -0800

    Update 262.sql

--- 262.sql
+++ 262.sql
@@ -1,5 +1,12 @@
 --#262. Trips and Users
 /*
-Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+Write a SQL query to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
 
 */
+SELECT request_at AS Day, ROUND(avg(Status!='completed'), 2) AS 'Cancellation Rate'
+FROM Trips t JOIN Users u1 ON (t.client_id = u1.users_id AND u1.banned = 'No')
+    JOIN Users u2 ON (t.driver_id = u2.users_id AND u2.banned = 'No')
+WHERE	
+    request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY 
+    request_at
