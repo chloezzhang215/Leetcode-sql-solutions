b98bb78ab61b19acffd081fd09c77845fab673bf
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-10 23:09:34 -0800

    Update 1978. 上级经理已离职的公司员工.sql

--- 1978. 上级经理已离职的公司员工.sql
+++ 1978. 上级经理已离职的公司员工.sql
@@ -1,1 +1,9 @@
+--#1978. 上级经理已离职的公司员工
+/*
+Write an SQL query to report the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company.
+*/
 
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id
