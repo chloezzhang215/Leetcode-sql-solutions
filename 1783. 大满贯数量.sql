926774b973f7c229aacdfd1b672fd54b00c50443
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-12 22:11:40 -0800

    Update 1783. 大满贯数量.sql

--- 1783. 大满贯数量.sql
+++ 1783. 大满贯数量.sql
@@ -3,7 +3,14 @@
 Write an SQL query to report the number of grand slam tournaments won by each player. Do not include the players who did not win any tournament.
 
 */
-
-/*
-窗口函数lead() over() 注意lead/lag这两个窗口函数，里面本身有三个参数 lead/lag ( field(选择的字段),num(偏移的个数),default(默认值，即无数据时的填充值)
-*/
+SELECT P.player_id, P.player_name, COUNT(*) AS grand_slams_count
+FROM Players P RIGHT JOIN
+(SELECT Wimbledon AS player_id FROM Championships
+UNION ALL
+SELECT Fr_open FROM Championships
+UNION ALL
+SELECT US_open FROM Championships
+UNION ALL
+SELECT Au_open FROM Championships) tmp
+ON (tmp.player_id = P.player_id)
+GROUP BY P.player_id
