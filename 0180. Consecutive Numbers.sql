83bd96f63b1ad5ad43436e5a3e02cd47d442213b
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-31 15:42:06 -0800

    Update 0180. Consecutive Numbers.sql

--- 0180. Consecutive Numbers.sql
+++ 0180. Consecutive Numbers.sql
@@ -7,3 +7,10 @@ Write an SQL query to find all numbers that appear at least three times consecut
 SELECT DISTINCT a.num AS ConsecutiveNums
 FROM Logs a,Logs b,Logs c
 WHERE a.id = b.id-1 AND b.id = c.id-1 AND a.num = b.num AND b.num = c.num
+
+
+SELECT DISTINCT num AS ConsecutiveNums FROM
+(SELECT *, COUNT(*) FROM
+(SELECT *, row_number()over(order by id)-row_number()over(partition by num order by id) AS ranking FROM Logs)a
+GROUP BY num,ranking
+HAVING COUNT(*)>=3)b
