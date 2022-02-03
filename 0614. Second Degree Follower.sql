26f9369c8e015a445eee0070d2f60f6c02cc5ae7
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-02-03 15:32:39 -0800

    Update and rename 614.sql to 0614. Second Degree Follower.sql

--- 0614. Second Degree Follower.sql
+++ 0614. Second Degree Follower.sql
@@ -3,7 +3,8 @@
 Write an SQL query to report the second-degree users and the number of their followers.
 
 */
-SELECT followee AS follower,COUNT(DISTINCT follower) AS num
-FROM follow
-WHERE followee IN (SELECT follower FROM follow)
+SELECT followee AS follower, COUNT(DISTINCT follower) AS num
+FROM Follow
+WHERE followee IN (SELECT follower FROM Follow)
 GROUP BY followee
+ORDER BY follower
