7654e51ee6ed3e9b0bd28a77c8f0ffcbba1c791e
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-02-03 16:51:03 -0800

    Update 0618. Students Report By Geography.sql

--- 0618. Students Report By Geography.sql
+++ 0618. Students Report By Geography.sql
@@ -1,1 +1,7 @@
-
+SELECT MAX(CASE continent WHEN 'America' THEN name ELSE NULL END)AS America,
+        MAX(CASE continent WHEN 'Asia' THEN name ELSE NULL END)AS Asia,
+        MAX(CASE continent WHEN 'Europe' THEN name ELSE NULL END)AS Europe
+FROM(
+SELECT *, row_number()over(partition by continent order by name)AS ranking FROM Student
+)t1
+GROUP BY ranking
