0245275d699edb56f8b6324161694c4ae183d144
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-02-05 15:58:13 -0800

    Create 1107. New Users Daily Count.sql

---
+++ 1107. New Users Daily Count.sql
@@ -0,0 +1,7 @@
+SELECT login_date, COUNT(user_id) AS user_count
+FROM
+(SELECT user_id, activity, MIN(activity_date) AS login_date FROM Traffic
+WHERE activity = 'login'
+GROUP BY user_id)A
+WHERE DATEDIFF('2019-06-30',login_date)<=90
+GROUP BY login_date
