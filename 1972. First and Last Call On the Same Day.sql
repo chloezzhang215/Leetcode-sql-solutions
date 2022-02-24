f350b3a7dbac0714ac8cebe83e58a295b4b0a111
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-02-24 15:45:43 -0800

    Update 1972. First and Last Call On the Same Day.sql

--- 1972. First and Last Call On the Same Day.sql
+++ 1972. First and Last Call On the Same Day.sql
@@ -1,14 +1,16 @@
-with temp as (
-select caller_id as id,recipient_id as id2,call_time, date_format(call_time,"%Y-%m-%d") as dt from calls
-union all
-select recipient_id as id,caller_id as id2,call_time, date_format(call_time,"%Y-%m-%d") as dt from calls
-),temp1 as (
-select id,id2, rank() over (partition by id, dt order by call_time desc) as rnk1,rank() over (partition by id, dt order by call_time ) as rnk2
-    from temp
+# Write your MySQL query statement below
+WITH t AS(
+    SELECT caller_id AS id1, recipient_id AS id2, call_time, DATE_FORMAT(call_time,'%Y-%m-%d') AS day FROM Calls
+    UNION ALL
+    SELECT recipient_id AS id1, caller_id AS id2, call_time, DATE_FORMAT(call_time,'%Y-%m-%d') AS day FROM Calls
+),
+t1 AS(
+    SELECT id1, id2, day, rank()over(partition by id1, day order by call_time) AS rnk1,
+           rank()over(partition by id1, day order by call_time desc) AS rnk2
+    FROM t
 )
-select distinct a.id as user_id
-from temp1 a, temp1 b
-where a.id=b.id
-and a.id2=b.id2
-and a.rnk1=1
-and b.rnk2=1
+
+SELECT DISTINCT id1 AS user_id FROM t1
+WHERE rnk1 = 1 OR rnk2 = 1
+GROUP BY id1, day
+HAVING COUNT(DISTINCT id2) = 1
